0814a1519450e339603c5d880249359ad0e4c4e1
Aliaksander Hutsau <aleksan9ep@gmail.com>
2019-02-01 03:52:55 +0300

    Добавьте файлы проекта.

---
+++ TexodeApp.Server/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TexodeApp.Server.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Base64Image { get; set; } = string.Empty;
+    }
+}
